7b9834d9225d84c6a9e400ac7427fb833b1b704e
Goastio <seventylife@gmail.com>
2022-09-05 12:25:20 -0400

    css adjustments

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -3,11 +3,15 @@ import React from "react";
 const Generator = () => {
   return (
     <>
-      <div className="flex min-h-screen flex-col items-center justify-center">
-        <div className="flex text-3xl text-white">Password Generator</div>
-        <div className="my-10 flex w-full max-w-screen-sm justify-center rounded-sm bg-neutral-700 p-20 text-white">
-          <div className="w-3/4 rounded-sm bg-neutral-600">
+      <div className="mx-auto flex min-h-screen max-w-screen-sm flex-col justify-center p-10 text-white">
+        <div className="flex self-center text-xl ">Password Generator</div>
+
+        <div className="my-10 flex flex-col gap-5">
+          <div className="w-full justify-center rounded-sm bg-neutral-600 p-5 text-center">
             This is where the password will generate.
+          </div>
+          <div className="flex w-full justify-center rounded-sm bg-neutral-700 p-20">
+            testing
           </div>
         </div>
       </div>
