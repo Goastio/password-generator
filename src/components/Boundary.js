3673f28a58decfe312bfc7f8d2c3819afb85f158
Goastio <seventylife@gmail.com>
2022-09-11 09:57:31 -0400

    added some funcs

--- src/components/Boundary.js
+++ src/components/Boundary.js
@@ -14,16 +14,6 @@ const Boundary = ({
   currentLength,
   setCurrentLength,
 }) => {
-  useEffect(() => {
-    randomUppercaseLetter();
-  }, []);
-
-  useEffect(() => {
-    if (uppercaseLetter && lowercaseLetter && numbers && symbols) {
-      setPassword(uppercaseLetter);
-    }
-  }, [uppercaseLetter, lowercaseLetter, numbers, symbols]);
-
   const randomUppercaseLetter = () => {
     for (let i = 0; i < 10; i++) {
       const rando = String.fromCharCode(65 + Math.floor(Math.random() * 26));
@@ -31,9 +21,39 @@ const Boundary = ({
     }
   };
 
-  const randomLowercaseLetter = String.fromCharCode(
-    97 + Math.floor(Math.random() * 26)
-  );
+  const randomLowercaseLetter = () => {
+    for (let i = 0; i < 10; i++) {
+      const rando = String.fromCharCode(97 + Math.floor(Math.random() * 26));
+      setLowercaseLetter((prev) => [...prev, rando]);
+    }
+  };
+
+  const randomNumbers = () => {
+    for (let i = 0; i < 10; i++) {
+      const rando = String.fromCharCode(48 + Math.floor(Math.random() * 10));
+      setNumbers((prev) => [...prev, rando]);
+    }
+  };
+
+  const randomSymbols = () => {
+    for (let i = 0; i < 10; i++) {
+      const rando = String.fromCharCode(
+        33 + Math.floor(Math.random() * 31)
+      );
+      setSymbols((prev) => [...prev, rando]);
+      console.log(rando);
+    }
+  };
+
+  useEffect(() => {
+    if (uppercaseLetter && lowercaseLetter && numbers && symbols) {
+      setPassword(uppercaseLetter);
+    }
+  }, [uppercaseLetter, lowercaseLetter, numbers, symbols]);
+
+  useEffect(() => {
+    randomSymbols();
+  }, []);
 
   return <div>{password}</div>;
 };
